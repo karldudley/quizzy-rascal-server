02e40945fa61d24a635bf605b585ede128152c3d
Leehamb99 <leehamb99@gmail.com>
2022-10-10 12:00:41 +0100

    update api

--- api/questions.js
+++ api/questions.js
@@ -2,20 +2,23 @@ import { Express } from "express";
 import axios from "axios";
 
 async function getQuestionData() {
+
+    var roundData = []
+
     try{
         const response = await axios.get("https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple")
         const data = response.data.results
         for(let i = 0; i < 10; i++) {
-         {
-                const roundData = [data[i].question, data[i].incorrect_answers[0],data[i].incorrect_answers[1],data[i].incorrect_answers[2], data[i].correct_answer]
-                console.log(roundData)
-            }
-                // Need to add timer 
+         
+                roundData.push([data[i].question, data[i].incorrect_answers[0],data[i].incorrect_answers[1],data[i].incorrect_answers[2], data[i].correct_answer])
+                
+            
         }
+        return (roundData)
+
     }
     catch (error) {
         console.log(error)
     }
 }
 
-getQuestionData()
